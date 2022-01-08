336778695bfdcc0d91644a52cfd9667164e027bc
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-08 12:28:38 +0530

    dowloading file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
-/* eslint-disable @next/next/link-passhref */
-import Link from 'next/link';
-import * as React from 'react';
-import Svg from '../components/Svg';
-
+import React, { useState } from "react";
 export default function Home() {
+  const [name, setName] = useState("");
+  const svg = `<svg
+width="200"
+height="151"
+viewBox="0 0 300 "
+fill="none"
+xmlns="http://www.w3.org/2000/svg">
+<ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill="#FC4343" />
+<text x="90" y="80" fontFamily="Verdana" fontSize="35" fill="blue">
+  ${name}
+</text>
+</svg>`;
+  const saveDoc = () => {
+    const element = document.createElement("a");
+    let string = svg.replace("%PDF-1.3", "");
+    console.log(string);
+    const file = new Blob([string], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = `logo.svg`;
+    document.body.appendChild(element);
+    element.click();
+  };
   return (
-      <main className=" w-full h-full font-[sans-serif]">
-        <section className='bg-white w-full'>
-        <Svg name={'test'} />
-        </section>
-        <a href='/components/Svg.tsx' download>Click to download</a>
-      </main>
+    <main className=" w-full h-full font-[sans-serif]">
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <button onClick={saveDoc}>Download</button>
+    </main>
   );
 }

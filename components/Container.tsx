06c4450934dde9d60730c92869dde1503202a087
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-24 01:49:49 +0530

    changes made in new ui

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: any) {
   return (
-    <div className=" bg-[#1a1a1a]  flex  justify-center items-center-max w-[600px] ">
+    <div className=" bg-[#1a1a1a]  flex  justify-center rounded-xl p-2 items-center-max w-[600px] ">
       {children}
     </div>
   );

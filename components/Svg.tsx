4eeaf31117c6ec32ce6a0087155caa77357339ff
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-23 22:40:29 +0530

    changes made

--- components/Svg.tsx
+++ components/Svg.tsx
@@ -1,11 +1,22 @@
 export default function Svg({ name,color,nameColor,borderRadius }: any) {
   return (
-    <div className={`opacity-100 w-[10rem] h-[10rem] `} 
-    style={{backgroundColor:`${color}`,
-    borderRadius:`${borderRadius}px`
-  }}
-    >
-      <svg
+  //   <div className={`opacity-100 w-[10rem] h-[10rem] `} 
+  //   style={{backgroundColor:`${color}`,
+  //   borderRadius:`${borderRadius}px`
+  // }}
+  //   >
+  //     <svg
+  //     id="svg"
+
+  //       fill="none"
+  //       xmlns="http://www.w3.org/2000/svg">
+  //       <ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill={color} />
+  //       <text x="90" y="80" fontFamily="Verdana" fontSize="35" fill={nameColor}>
+  //         {name}
+  //       </text>
+  //     </svg>
+  //   </div>
+  <svg
       id="svg"
 
         fill="none"
@@ -15,6 +26,5 @@ export default function Svg({ name,color,nameColor,borderRadius }: any) {
           {name}
         </text>
       </svg>
-    </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import Svg from "../components/Svg";
 export default function Home() {
-  const testcolor ='#f00';
-  const [color,setColor] =useState('#FC4343')
-  const[nameColor,setNameColor] = useState('#Ffff')
-  const [range , setRange] = useState(0)
-  const[borderRadius , setBorderRadius] = useState('0')
+  const [color, setColor] = useState("#FC4343");
+  const [nameColor, setNameColor] = useState("#Ffff");
+  const [range, setRange] = useState(0);
+  const [borderRadius, setBorderRadius] = useState("0");
   const [name, setName] = useState("");
   const svg = `<svg
-            width="200"
-            height="151"
-            viewBox="0 0 300"
             fill="none"
             xmlns="http://www.w3.org/2000/svg">
             <ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill='${color}' />
@@ -29,20 +25,40 @@ export default function Home() {
     element.click();
   }
   return (
-    <main className=" w-full h-full font-[sans-serif]">
-      <Svg name={name} color={color} nameColor={nameColor} borderRadius={borderRadius}/>
-      <input
+    <main className=" w-full h-full flex flex-col  font-[sans-serif]">
+      <div>  <Svg
+        name={name}
+        color={color}
+        nameColor={nameColor}
+        borderRadius={borderRadius}
+      /></div>
+      <div>      <input
         type="text"
         value={name}
         className="bg-white border-2 border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
         onChange={(e) => setName(e.target.value)}
       />
-{color}
-<input type="color" value={color} onChange={e => setColor(e.target.value)} />
-{nameColor}
-<input type="color" value={nameColor} onChange={e => setNameColor(e.target.value)} />
-<input type="range" value={borderRadius} onChange={e => setBorderRadius(e.target.value)}/>
-{borderRadius}
+     </div>
+      <div>      <input
+        type="color"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
+      {nameColor}
+      <input
+        type="color"
+        value={nameColor}
+        onChange={(e) => setNameColor(e.target.value)}
+      />
+      <input
+        type="range"
+        value={borderRadius}
+        onChange={(e) => setBorderRadius(e.target.value)}
+      /></div>
+
+
+
+      {borderRadius}
       <button onClick={saveDoc}>Download</button>
     </main>
   );

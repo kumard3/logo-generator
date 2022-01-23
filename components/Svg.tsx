df0623f594cb4128adfa0a5232d0a43dac555c97
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-23 22:32:54 +0530

    working on ui

--- components/Svg.tsx
+++ components/Svg.tsx
@@ -1,14 +1,17 @@
-export default function Svg({ name }: any) {
+export default function Svg({ name,color,nameColor,borderRadius }: any) {
   return (
-    <div>
+    <div className={`opacity-100 w-[10rem] h-[10rem] `} 
+    style={{backgroundColor:`${color}`,
+    borderRadius:`${borderRadius}px`
+  }}
+    >
       <svg
-        width="200"
-        height="151"
-        viewBox="0 0 300 "
+      id="svg"
+
         fill="none"
         xmlns="http://www.w3.org/2000/svg">
-        <ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill="#FC4343" />
-        <text x="90" y="80" fontFamily="Verdana" fontSize="35" fill="blue">
+        <ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill={color} />
+        <text x="90" y="80" fontFamily="Verdana" fontSize="35" fill={nameColor}>
           {name}
         </text>
       </svg>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
+import Svg from "../components/Svg";
 export default function Home() {
+  const testcolor ='#f00';
+  const [color,setColor] =useState('#FC4343')
+  const[nameColor,setNameColor] = useState('#Ffff')
+  const [range , setRange] = useState(0)
+  const[borderRadius , setBorderRadius] = useState('0')
   const [name, setName] = useState("");
   const svg = `<svg
-width="200"
-height="151"
-viewBox="0 0 300 "
-fill="none"
-xmlns="http://www.w3.org/2000/svg">
-<ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill="#FC4343" />
-<text x="90" y="80" fontFamily="Verdana" fontSize="35" fill="blue">
-  ${name}
-</text>
-</svg>`;
-  const saveDoc = () => {
+            width="200"
+            height="151"
+            viewBox="0 0 300"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill='${color}' />
+            <text x="90" y="80" font-family="Nunito Sans" font-size="35" fill='${nameColor}'>
+            ${name}
+            </text>
+            </svg>`;
+  function saveDoc() {
     const element = document.createElement("a");
     const file = new Blob([svg], {
       type: "text/plain",
@@ -21,15 +27,22 @@ xmlns="http://www.w3.org/2000/svg">
     element.download = `logo.svg`;
     document.body.appendChild(element);
     element.click();
-  };
+  }
   return (
     <main className=" w-full h-full font-[sans-serif]">
+      <Svg name={name} color={color} nameColor={nameColor} borderRadius={borderRadius}/>
       <input
         type="text"
         value={name}
+        className="bg-white border-2 border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
         onChange={(e) => setName(e.target.value)}
       />
-
+{color}
+<input type="color" value={color} onChange={e => setColor(e.target.value)} />
+{nameColor}
+<input type="color" value={nameColor} onChange={e => setNameColor(e.target.value)} />
+<input type="range" value={borderRadius} onChange={e => setBorderRadius(e.target.value)}/>
+{borderRadius}
       <button onClick={saveDoc}>Download</button>
     </main>
   );

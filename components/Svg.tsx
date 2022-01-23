ca423ebb00e5f9a9591bf88849c542b4ae014333
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-23 23:23:36 +0530

    changes made

--- components/Svg.tsx
+++ components/Svg.tsx
@@ -25,16 +25,16 @@ export default function Svg({
     //   </div>
     <svg
       id="svg"
-      style={{
-        backgroundColor: `${color}`,
-        borderRadius: `${borderRadius}px`,
-        width: `${width}px`,
-        height: `${height}px`,
-      }}
+      // style={{
+      //   backgroundColor: `${color}`,
+      //   borderRadius: `${borderRadius}px`,
+      //   width: `${width}px`,
+      //   height: `${height}px`,
+      // }}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      {/* <ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill={color} /> */}
+      <ellipse cx="75" cy="75.5" rx="75" ry="75.5" fill={color} />
       <text x="90" y="80" fontFamily="Verdana" fontSize="35" fill={nameColor}>
         {name}
       </text>

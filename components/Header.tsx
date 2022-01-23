eca9656dad9d9dd07ade80b520f7e556c4afa0d2
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-23 23:33:14 +0530

    working on ui

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Header() {
+  return( <div className="text-5xl text-center ">
+      Logo Generator
+  </div>)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import Header from "../components/Header";
 import Svg from "../components/Svg";
 export default function Home() {
   const [color, setColor] = useState("#FC4343");
   const [nameColor, setNameColor] = useState("#Ffff");
   const [range, setRange] = useState(0);
-  const[width ,setWidth] = useState("0")
-  const[height,setHeight] = useState("0")
+  const [width, setWidth] = useState("0");
+  const [height, setHeight] = useState("0");
   const [borderRadius, setBorderRadius] = useState("0");
   const [name, setName] = useState("");
   const svg = `<svg
@@ -28,54 +29,64 @@ export default function Home() {
   }
   return (
     <main className=" w-full h-full flex flex-col  font-[sans-serif]">
-      <div>  <Svg
-        name={name}
-        color={color}
-        nameColor={nameColor}
-        borderRadius={borderRadius}
-        width={width}
-        height={height}
-      /></div>
-      <div>      <input
-        type="text"
-        value={name}
-        className="bg-white border-2 border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-        onChange={(e) => setName(e.target.value)}
-      />
-     </div>
-      <div>      
+      <Header />
+      <div>
+        {" "}
+        <Svg
+          name={name}
+          color={color}
+          nameColor={nameColor}
+          borderRadius={borderRadius}
+          width={width}
+          height={height}
+        />
+      </div>
+      <div>
+        {" "}
         <input
-        type="color"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-      />
-      {nameColor}
-      <input
-        type="color"
-        value={nameColor}
-        onChange={(e) => setNameColor(e.target.value)}
-      />
-      <input
-        type="range"
-        value={borderRadius}
-        onChange={(e) => setBorderRadius(e.target.value)}
-      />      
-      <input
-        type="range"
-        min="0" max="1000"
-        value={width}
-        onChange={(e) => setWidth(e.target.value)}
-      />      <input
-        type="range"
-        min="0" max="1000"
-        value={height}
-        onChange={(e) => setHeight(e.target.value)}
-      />
-      
+          type="text"
+          value={name}
+          className="bg-white border-2 border-gray-400 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        {nameColor}
+        <input
+          type="color"
+          value={nameColor}
+          onChange={(e) => setNameColor(e.target.value)}
+        />
+        <input
+          type="range"
+          value={borderRadius}
+          onChange={(e) => setBorderRadius(e.target.value)}
+        />
+        <input
+          type="range"
+          min="0"
+          max="1000"
+          value={width}
+          onChange={(e) => setWidth(e.target.value)}
+        />{" "}
+        <input
+          type="range"
+          min="0"
+          max="1000"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+        />
       </div>
       {borderRadius}
       {width}
-      <button onClick={saveDoc}>Download</button>
+      <div className="flex justify-center items-center">
+      <button className="border-2 border-black px-3 py-2 rounded-xl "  onClick={saveDoc}>Download</button>
+         </div>
     </main>
   );
 }
